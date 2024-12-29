7d04337a703492c6ce2d4e00ec4a125992a815b0
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-12-29 12:10:58 +0000

    feat: cognito

--- terraform/modules/terraform-aws-cognito/main.tf
+++ terraform/modules/terraform-aws-cognito/main.tf
@@ -1,10 +1,3 @@
 resource "aws_cognito_user_pool" "pool" {
   name = "Lab user pool"
-
-  account_recovery_setting {
-    recovery_mechanism {
-      name = "verified_email"
-      priority = 1
-    }
-  }
 }
