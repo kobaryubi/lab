ec4a880694341f34cbe42adee0da79ce2cd876d1
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-12-29 13:46:59 +0000

    feat: validity

--- terraform/modules/terraform-aws-cognito/main.tf
+++ terraform/modules/terraform-aws-cognito/main.tf
@@ -12,7 +12,17 @@ resource "aws_cognito_user_pool" "pool" {
 resource "aws_cognito_user_pool_client" "client" {
   name                          = "Lab web app"
   user_pool_id                  = aws_cognito_user_pool.pool.id
+  access_token_validity         = 60
   enable_token_revocation       = true
-  explicit_auth_flows           = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+  explicit_auth_flows           = ["ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+  generate_secret               = true
+  id_token_validity             = 60
   prevent_user_existence_errors = "ENABLED"
+  refresh_token_validity        = 5
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "days"
+  }
 }
