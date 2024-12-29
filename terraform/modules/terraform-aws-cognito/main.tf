f09e1fad0b622cd5dd997351684e41a6f925e351
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-12-29 12:29:24 +0000

    fix: type

--- terraform/modules/terraform-aws-cognito/main.tf
+++ terraform/modules/terraform-aws-cognito/main.tf
@@ -1,5 +1,5 @@
 resource "aws_cognito_user_pool" "pool" {
   name                = "Lab user pool"
-  alias_attributes    = "email"
+  alias_attributes    = ["email"]
   deletion_protection = "ACTIVE"
 }
