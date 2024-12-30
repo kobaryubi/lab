9054a9cc081eda37a936438a324afb08e8709ebd
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-12-30 07:59:34 +0000

    feat: controller

---
+++ laravel/app/Http/Controllers/ChirpController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('chirps.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('chirps.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        return redirect(route('chirps.index'));
+    }
+}
