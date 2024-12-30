15e9636b0c3c984b5ffdae361b2158662ba76f88
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-12-30 14:39:10 +0900

    feat: routes

---
+++ laravel/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\ChirpController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome', ['name' => 'James']);
+});
+
+Route::get('/chirps', [ChirpController::class, 'index'])->name('chirps.index');
+Route::get('/chirps/create', [ChirpController::class, 'create'])->name('chirps.create');
+Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.store');
