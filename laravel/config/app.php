c9574bedc597f4489c1001c04374da4804c8af07
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-12-31 00:47:22 +0000

    feat: config

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -2,4 +2,8 @@
 
 return [
     "name" => env("APP_NAME", "Laravel"),
+    "env" => env("APP_ENV", "production"),
+    "debug" => (bool) env("APP_DEBUG", false),
+    "url" => env("APP_URL", "http://localhost"),
+    "timezone" => env("APP_TIMEZONE", "UTC"),
 ];
