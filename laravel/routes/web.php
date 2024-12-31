9c3d1b1f1110eb962719c650f8fa1b38d6dfd145
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-12-31 12:00:49 +0000

    feat: dashboard

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
+Route::get("/dashboard", function () {
+    return view("dashboard");
+})->middleware(["auth", "verified"])->name("dashboard");
+
 Route::get('/chirps', [ChirpController::class, 'index'])->name('chirps.index');
 Route::get('/chirps/create', [ChirpController::class, 'create'])->name('chirps.create');
 Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.store');
+
+require __DIR__."/auth.php";
