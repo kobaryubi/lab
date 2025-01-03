a848766391b61d57d07d3c2671f3035b53136158
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-01-03 22:21:46 +0900

    feat: main

--- AndroidLab/app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
+++ AndroidLab/app/src/main/java/com/masahikokobayashi/androidlab/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -29,26 +32,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AndroidLabApp(modifier: Modifier = Modifier) {
+fun AndroidLabApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose"),
+) {
     Scaffold(modifier = modifier) { innerPadding ->
         Surface(
-            modifier = Modifier.padding(innerPadding),
-            color = MaterialTheme.colorScheme.background
+            modifier = Modifier.padding(innerPadding), color = MaterialTheme.colorScheme.background,
         ) {
-            Greeting(
-                name = "Android",
-            )
+            Column(modifier = Modifier.padding(vertical = 4.dp)) {
+                for (name in names) {
+                    Greeting(name = name)
+                }
+            }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+            }
+        }
     }
 }
 
